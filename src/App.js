065aa27dbc8f53df33a9f504da1712c306019604
Wojciech <heiw17@wp.pl>
2023-09-19 20:15:27 +0200

    commit-all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import "./App.css";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Link,
+  Outlet
+} from "react-router-dom";
+import Dashboard from "./components/dashbord/Dashboard";
+import Nav from "./components/layout/Nav";
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Nav />
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Dashboard />}></Route>
+    </Route>
+  )
+);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
