1ea135567390d66750c97682be93eb3b156ebdb8
Wojciech <heiw17@wp.pl>
2023-09-20 20:24:19 +0200

    a

---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ProjectSummary = () => {
+  return (
+    <div>
+      {" "}
+      <div
+        className="card z-depth-0 project-summary
+  "
+      >
+        <div className="card-content grey-text text-darken-3">
+          <span className="card-title">Project Title</span>
+          <p>Posted by WZ</p>
+          <p className="grey-text">20th september, 6pm</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
